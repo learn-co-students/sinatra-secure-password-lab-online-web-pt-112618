8a989e03d64d2e424429cd0a6084c0b267e93daa
norawolf <noraevogt@gmail.com>
2019-03-21 11:38:21 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,8 +17,12 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    #your code here
-
+    user = User.new(username: params[:username], password: params[:password])
+    if user.save && !user.username.empty?
+      redirect "/login"
+    else
+      redirect "/failure"
+    end
   end
 
   get '/account' do
@@ -33,17 +37,26 @@ class ApplicationController < Sinatra::Base
 
   post "/login" do
     ##your code here
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect "/account"
+    else
+      redirect "/failure"
+    end
   end
 
   get "/failure" do
     erb :failure
   end
 
+#clears session data and redirects to home page
   get "/logout" do
     session.clear
     redirect "/"
   end
 
+# views automatically has access to helper methods
   helpers do
     def logged_in?
       !!session[:user_id]
